2e644e860aa09adfecb362d49d6326cce30a1284
Sergey Savelev <sergo-5avelev@yandex.ru>
2020-09-16 22:35:25 +0300

    learn

--- myCod.js
+++ myCod.js
@@ -1,9 +1,8 @@
-let num = 1;
-		
-if (num == 1) {
-	 result = 'верно';
-} else {
-	 result = 'неверно';
-}
+let arr = ["пн", "вт", "ср", "чт", "пт", "сб", "вс"];
 
-console.log(result);
+for (let i = 0; i < arr.length; i++) {
+if(arr[i] == "сб" || arr[i] == "вс") {
+document.write('<b>' + arr[i] + '</b>' + '<br>')
+} else { document.write(arr[i] + '<br>')
+}
+}
